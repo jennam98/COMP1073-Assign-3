270c7b2adb42b91ca40626519c4c47d2d5bcd640
TeresaH16 <heredia.t@icloud.com>
2026-04-02 14:49:46 -0400

    const elements added to js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-//API_KEY
+const todoInput = document.getElementById("todoInput");
+const addBtn = document.getElementById("addBtn");
+const todoList = document.getElementById("todoList");
+const totalTasks = document.getElementById("totalTasks");
+const completedTasks = document.getElementById("completedTasks");
+const remainingTasks = document.getElementById("remainingTasks");
+const emptyMessage = document.getElementById("emptyMessage");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
+const dingSound = document.getElementById("dingSound");
+const filterButtons = document.querySelectorAll(".filter-btn");
